ab45475cb894d62d16e0fd6ee92f4f2a4f112f94
JoseH <josehm1999@gmail.com>
2023-10-26 00:20:54 -0500

    Added company model

---
+++ src/models/company.ts
@@ -0,0 +1,8 @@
+class Company {
+    private ruc: string | null  = null;
+    private razonSocial: string | null = null;
+    private nombreComercial: string | null = null;
+    private address:
+
+
+}
